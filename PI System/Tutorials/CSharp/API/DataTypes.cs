abe23dab71889ae06c43b1d57bd5684d8c8d3165
Dmitriy Shvidchenko <DShvidchenko@osisoft.com>
2018-04-18 12:13:49 -0400

    Initial set of samples and the readme for the Tutorials location.
    Some more changes are coming - once we have all documentation links in place, will update the comments and readme.

---
+++ PI System/Tutorials/CSharp/API/DataTypes.cs
@@ -0,0 +1,207 @@
+﻿/*************************************************************************************
+# Copyright 2018 OSIsoft, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# <http://www.apache.org/licenses/LICENSE-2.0>
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NOTE: this script was designed using the v1.0
+# version of the OMF specification, as outlined here:
+# http://omf-docs.osisoft.com/en/v1.0
+****************************************************************************************/
+
+using System;
+
+namespace IngressServiceAPI
+{
+    /// <summary>
+    /// Sample data structure to represent data objects in the target system.
+    /// </summary>
+    /// 
+
+    class FirstStaticType
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string StringProperty { get; set; }
+
+        public const string JsonSchema =
+            @"{
+                ""id"": ""FirstStaticType"",
+                ""name"": ""First static type"",
+                ""classification"": ""static"",
+                ""type"": ""object"",
+                ""description"": ""First static asset type"",
+                ""properties"": {
+                    ""index"": {
+                        ""type"": ""string"",
+                        ""isindex"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""name"": {
+                        ""type"": ""string"",
+                        ""isname"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""StringProperty"": {
+                        ""type"": ""string"",
+                        ""name"": ""First configuration attribute"",
+                        ""description"": ""First static asset type's configuration attribute""
+                    }
+                }
+            }";
+    }
+
+    class SecondStaticType
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string StringProperty { get; set; }
+
+        public const string JsonSchema =
+            @"{
+                ""id"": ""SecondStaticType"",
+                ""name"": ""Second static type"",
+                ""classification"": ""static"",
+                ""type"": ""object"",
+                ""description"": ""Second static asset type"",
+                ""properties"": {
+                    ""index"": {
+                        ""type"": ""string"",
+                        ""isindex"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""name"": {
+                        ""type"": ""string"",
+                        ""isname"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""StringProperty"": {
+                        ""type"": ""string"",
+                        ""name"": ""Second configuration attribute"",
+                        ""description"": ""Second static asset type's configuration attribute""
+                    }
+                }
+            }";
+    }
+
+
+    class FirstDynamicType
+    {
+        public DateTime timestamp { get; set; }
+        public int IntegerProperty { get; set; }
+
+        public const string JsonSchema =
+            @"{
+               ""id"": ""FirstDynamicType"",
+                ""name"": ""First dynamic type"",
+                ""classification"": ""dynamic"",
+                ""type"": ""object"",
+                ""description"": ""not in use"",
+                ""properties"": {
+                    ""timestamp"": {
+                        ""format"": ""date-time"",
+                        ""type"": ""string"",
+                        ""isindex"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""IntegerProperty"": {
+                        ""type"": ""integer"",
+                        ""name"": ""Integer attribute"",
+                        ""description"": ""PI point data referenced integer attribute""
+                    }
+                }
+            }";
+    }
+
+    class SecondDynamicType
+    {
+        public DateTime timestamp { get; set; }
+        public double  NumberProperty1 { get; set; }
+        public double NumberProperty2 { get; set; }
+        public string StringEnum { get; set; }
+
+
+        public const string JsonSchema =
+            @"{
+               ""id"": ""SecondDynamicType"",
+                ""name"": ""Second dynamic type"",
+                ""classification"": ""dynamic"",
+                ""type"": ""object"",
+                ""description"": ""not in use"",
+                ""properties"": {
+                    ""timestamp"": {
+                        ""format"": ""date-time"",
+                        ""type"": ""string"",
+                        ""isindex"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""NumberProperty1"": {
+                        ""type"": ""number"",
+                        ""name"": ""Number attribute 1"",
+                        ""description"": ""PI point data referenced number attribute 1"",
+                        ""format"": ""float64""
+                    },
+                    ""NumberProperty2"": {
+                        ""type"": ""number"",
+                        ""name"": ""Number attribute 2"",
+                        ""description"": ""PI point data referenced number attribute 2"",
+                        ""format"": ""float64""
+                    },
+                    ""StringEnum"": {
+                        ""type"": ""string"",
+                        ""enum"": [""False"", ""True""],
+                        ""name"": ""String enumeration representing Boolean type"",
+                        ""description"": ""PI point data referenced digital set for Boolean"",
+                    }
+                }
+            }";
+    }
+
+
+    class ThirdDynamicType
+    {
+        public DateTime timestamp { get; set; }
+        public int IntegerEnum { get; set; }
+
+        public const string JsonSchema =
+            @"{
+               ""id"": ""ThirdDynamicType"",
+                ""name"": ""Integer enumeration"",
+                ""classification"": ""dynamic"",
+                ""type"": ""object"",
+                ""description"": ""not in use"",
+                ""properties"": {
+                    ""timestamp"": {
+                        ""format"": ""date-time"",
+                        ""type"": ""string"",
+                        ""isindex"": true,
+                        ""name"": ""not in use"",
+                        ""description"": ""not in use""
+                    },
+                    ""IntegerEnum"": {
+                        ""type"": ""integer"",
+                        ""name"": ""Integer enumeration"",
+                        ""enum"": [0, 1],
+                        ""description"": ""Integer enumeration to replace boolean type"",
+                        ""format"": ""int16""
+                    }
+                }
+            }";
+    }
+
+}
--- PI System/Tutorials/README.md
+++ PI System/Tutorials/README.md
@@ -1,1 +1,5 @@
-Coming soon!
+# OMF-Samples for PI Connector Relay and PI System backend
+
+Here you may find three OMF v1.0 sample applications written in Microsoft C#, Python and Node.js. The samples demonstrate the most common usage of OMF version 1.0 with the PI Connector Relay ingress endpoint, and PI System backend. They all create the same PI Asset Framework reference models and the same sets of PI Data Archive points. The only differnce is the programming language been used.
+
+Before you run the samples, you need to register them with PI Connector Administator to obtain necessary Producer Token and Relay Ingress URL, which you will use to send OMF messages. For more information, see PI Connector Administation Guide.
