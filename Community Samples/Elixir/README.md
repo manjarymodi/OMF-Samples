20590d735ee72b918764d1c82e13147303e1f45e
Manjary modi <mmodi@osisoft.com>
2018-06-04 11:40:04 -0700

    Elixir sample is added in community Samples

---
+++ Community Samples/Elixir/README.md
@@ -0,0 +1,12 @@
+#Steps to run this Sample :
+#       1. Install elixir 
+#       2. Import this project project
+#       3. Go to command prompt.
+#       4. Run commands 
+#           -> mix deps.get             (This command gets the dependent libraries)
+#           -> mix clean                (In order to clean the existing exe files)
+#           -> mix escript.build        (To build the project)
+#           -> escript sample           (To run the executable)
+
+# Inorder to make the API call, this sample is using httpotion library. Inorder to learn more about elixir and httption please refer https://hexdocs.pm/elixir/1.0.5
+
--- Community Samples/Elixir/lib/sample.ex
+++ Community Samples/Elixir/lib/sample.ex
@@ -1,7 +1,10 @@
 defmodule Sample do
     def main(args) do
-    
-        #response = HTTPotion.post "https://httpbin.org/post", [body: "hello=" <> URI.encode_www_form("w o r l d !!"), headers: ["User-Agent": "My App", "Content-Type": "application/x-www-form-urlencoded"]]
+        # Replace userToken with your producer token       
+        userToken  =  "1/c97551d6bf784d1187f41d6f6c050267/9f05d6d2e914419898ec639a0657af61/365cc376-d621-41b0-b32f-7a982b22a63c/63671337180/5HLD7D%2FIbVui3BELRSVs8Pbc2%2F8X1%2Ba9X319biBXx8w%3D"       
+        # Replace omfEndPoint with your OMF END POINT 
+        omfEndPoint = "https://dat-a.osisoft.com/api/omf"
+      
         typeBody = "[{
         \"id\": \"simple_data_type\",
         \"description\": \"A simple number + timestamp object\",
@@ -19,29 +22,26 @@ defmodule Sample do
                 }
                      }
             }]"
-
-        IO.puts "******************************** Creating simple type ***************************************************************"
-       
+           
         IO.inspect typeBody    
 
         IO.puts "******************************** Creating Simple type ***************************************************************"
-        typeResponse = HTTPotion.post "https://dat-a.osisoft.com/api/omf", 
+        typeResponse = HTTPotion.post omfEndPoint, 
         [
             body: typeBody, 
             headers: 
                 ["Accept": "application/json", 
                 "Accept-Charset": "UTF-8" , 
                 "Content-Type": "application/json;charset=UTF-8", 
-                "producertoken": "1/c97551d6bf784d1187f41d6f6c050267/9f05d6d2e914419898ec639a0657af61/365cc376-d621-41b0-b32f-7a982b22a63c/63671337180/5HLD7D%2FIbVui3BELRSVs8Pbc2%2F8X1%2Ba9X319biBXx8w%3D", 
+                "producertoken": userToken, 
                 "messageformat": "JSON", 
                 "messagetype": "Type", 
                 "omfversion": "1.0"
                 ]
         ]
+
         IO.inspect typeResponse
-        json = Poison.Parser.parse!(typeBody)
-        #IO.inspect json["headers"]
-        IO.inspect json
+        
         IO.puts "******************************** Simple Type Created ***************************************************************"
 
         IO.puts "******************************** Creating Simple container ***************************************************************"
@@ -51,32 +51,29 @@ defmodule Sample do
         \"typeid\": \"simple_type\"
             }]"
 
-        containerResponse = HTTPotion.post "https://dat-a.osisoft.com/api/omf", 
+        containerResponse = HTTPotion.post omfEndPoint, 
         [
             body: containerBody, 
             headers: 
                 ["Accept": "application/json", 
                 "Accept-Charset": "UTF-8" , 
                 "Content-Type": "application/json;charset=UTF-8", 
-                "producertoken": "1/c97551d6bf784d1187f41d6f6c050267/9f05d6d2e914419898ec639a0657af61/365cc376-d621-41b0-b32f-7a982b22a63c/63671337180/5HLD7D%2FIbVui3BELRSVs8Pbc2%2F8X1%2Ba9X319biBXx8w%3D", 
+                "producertoken": userToken, 
                 "messageformat": "JSON", 
                 "messagetype": "container", 
                 "omfversion": "1.0"
                 ]
         ]
-
-
-       
+      
         IO.puts "******************************** Simple Container Created ***************************************************************"
-
         IO.inspect containerResponse
         IO.puts "******************************** Sending the simple data ***************************************************************"
 
+        #Method call to send data to SDS 
+        omf_data(userToken, omfEndPoint)
       
-        omf_data()
-      
- 
-     IO.puts "********************************Creating Complex Type **************************************************************"
+        #For complex data, only type is generated but data is not sending.
+        IO.puts "********************************Creating Complex Type **************************************************************"
         complexTypeBody = "[{
             \"id\": \"complex_type1\",
             \"description\": \"A complex type including strings, ints, doubles, arrays, and nested objects\",
@@ -131,34 +128,32 @@ defmodule Sample do
                 }
          }]"
 
-        IO.puts "******************************** Creating Simple type ***************************************************************"
-        complexTypeResponse =  HTTPotion.post "https://dat-a.osisoft.com/api/omf", 
+        complexTypeResponse =  HTTPotion.post omfEndPoint, 
         [
             body: complexTypeBody, 
             headers: 
                 ["Accept": "application/json", 
                 "Accept-Charset": "UTF-8" , 
                 "Content-Type": "application/json;charset=UTF-8", 
-                "producertoken": "1/c97551d6bf784d1187f41d6f6c050267/9f05d6d2e914419898ec639a0657af61/365cc376-d621-41b0-b32f-7a982b22a63c/63671337180/5HLD7D%2FIbVui3BELRSVs8Pbc2%2F8X1%2Ba9X319biBXx8w%3D", 
+                "producertoken": userToken, 
                 "messageformat": "JSON", 
                 "messagetype": "Type", 
                 "omfversion": "1.0"
                 ]
         ]
         IO.inspect complexTypeResponse
-        json = Poison.Parser.parse!(complexTypeBody)
-        #IO.inspect json["headers"]
-        IO.inspect json
+        
         IO.puts "******************************** Complex Type created ***************************************************************"
 
 
     end
 
-    def omf_data() do
+    #Method to send data to SDS 
+    def omf_data(userToken, omfEndPoint) do
       
         Enum.each(0..99, fn(x) ->
         dateTime = Time.to_string(Time.utc_now())
-        IO.inspect  "***********************************I am inserting #{dateTime}"
+        IO.inspect  " Inserting index  --  #{dateTime}"
         dataBody = "[{
         \"containerid\": \"simple_data_stream\",
         \"values\": [{
@@ -167,14 +162,14 @@ defmodule Sample do
          }]
         }]"
 
-        dataResponse = HTTPotion.post "https://dat-a.osisoft.com/api/omf", 
+        dataResponse = HTTPotion.post omfEndPoint, 
         [
             body: dataBody, 
             headers: 
                 ["Accept": "application/json", 
                 "Accept-Charset": "UTF-8" , 
                 "Content-Type": "application/json;charset=UTF-8", 
-                "producertoken": "1/c97551d6bf784d1187f41d6f6c050267/9f05d6d2e914419898ec639a0657af61/365cc376-d621-41b0-b32f-7a982b22a63c/63671337180/5HLD7D%2FIbVui3BELRSVs8Pbc2%2F8X1%2Ba9X319biBXx8w%3D", 
+                "producertoken": userToken, 
                 "messageformat": "JSON", 
                 "messagetype": "data", 
                 "omfversion": "1.0"
